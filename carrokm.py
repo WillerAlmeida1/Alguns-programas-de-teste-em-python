628dbb6754777a8ab522b47f9d9f355f4eb92021
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-02-09 23:10:09 -0300

    calcula media de combustivel

---
+++ carrokm.py
@@ -0,0 +1,17 @@
+
+km_anterior = float (input("Coloque aqui a quilometragem anterior: "))
+km_atual = float (input("Coloqye aqui a sua quilometragem atual: "))
+gasto_litro = float (input("Qual a quantidade de combustivel gasto para percorrer esse percurso: "))
+
+km_atual = km_atual - km_anterior
+km_percorrida = km_atual
+gasto = km_atual / gasto_litro
+
+print("Você está a "+ str(km_percorrida) +"km de distância do ponto anterior e o gasto foi de: " + str(gasto) + "km/l")
+
+#dist = float (input("Coloque aqui a distância percorrida: "))
+#gas = float (input("Coloque aqui o gasto em litros: "))
+
+#gasto = dist / gas
+
+#print(gasto)
